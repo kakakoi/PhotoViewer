5623a0d7574c3eaced1fc88468fa5f48d30bdf5e
kakakoi <56661650kakakoi@users.noreply.github.com>
2021-03-18 12:09:52 +0900

    first commit

---
+++ app/src/main/java/com/kakakoi/photoviewer/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.kakakoi.photoviewer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kakakoi.photoviewer.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
