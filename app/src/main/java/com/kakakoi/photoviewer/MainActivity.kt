58a4f6722e0dc00f27d5849bd97122beb90b0b28
kakakoi <56661650+kakakoi@users.noreply.github.com>
2021-03-19 17:45:08 +0900

    #5 Add AppBar

--- app/src/main/java/com/kakakoi/photoviewer/MainActivity.kt
+++ app/src/main/java/com/kakakoi/photoviewer/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.kakakoi.photoviewer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import com.kakakoi.photoviewer.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
@@ -9,10 +11,33 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+        setSupportActionBar(findViewById(R.id.toolbar))
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+
+        val inflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.action_settings -> {
+            true
+        }
+
+        R.id.action_log -> {
+            true
+        }
+
+        else -> {
+            super.onOptionsItemSelected(item)
         }
     }
 }
